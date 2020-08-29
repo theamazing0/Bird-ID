e2d2707b9c400abbf1519f260222ce293f1df48d
theAmazingByte <ksamvid@gmail.com>
2020-08-29 10:04:08 -0400

    Initial Commit

---
+++ Bird ID/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bird ID
+//
+//  Created by Samvid Konchada on 8/29/20.
+//  Copyright © 2020 Samvid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
